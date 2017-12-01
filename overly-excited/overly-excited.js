ef58ef26963bf4eead8576ac427931010d702724
Valerie Freeman <valerah7@gmail.com>
2017-12-01 13:47:34 -0600

    Adding conditions to the loop

--- overly-excited/overly-excited.js
+++ overly-excited/overly-excited.js
@@ -10,22 +10,22 @@ function addExcitement (theWordArray) {
     // console.log(sentence.join(" "));
     // console.log(sentence);
     
-    //First try using array
-    for (let i = 0, newWordArray = []; i < theWordArray.length; i++) {
-        newWordArray.push(theWordArray[i]);
-        console.log(newWordArray.join(" "));
+        //First try using array
+    // for (let i = 0, newWordArray = []; i < theWordArray.length; i++) {
+    //     newWordArray.push(theWordArray[i]);
+    //     console.log(newWordArray.join(" "));
 
-    }
+    // }
     
-    //Second try using string concatenation
-    // for (let i = 0, string = ""; i < theWordArray.length; i++) {
-    //     string = string + theWordArray[i] + " ";
+        //Second try using string concatenation
+    for (let i = 0, string = ""; i < theWordArray.length; i++) {
+        string = string + " " + theWordArray[i];
         // If the current value of the counter variable can be evenly divided by 3 - using the JavaScript remainder operator - then add a single exclamation point (!) to the output.
-    //    if (i != 0 && i % 3 == 0) {
-    //     newWordArray.push('!');
-    // }
-    //     console.log(string);
-    // }
+       if ((i + 1) % 3 == 0) {
+        string = string + "!";
+        }
+        console.log(string);
+    }
 
     
 
